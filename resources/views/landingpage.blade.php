ff4bc35fae44c1082b020ffb1a503cd1405c1709
Firshada Jusaim <firshadajusaim@gmail.com>
2023-11-21 00:03:52 +0700

    Initial commit

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,248 @@
+@extends('layouts.main')
+
+
+@section('container')
+    <!-- Carousel -->
+    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+        <!-- Indicators/dots -->
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+            <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+        </div>
+
+        <!-- The slideshow/carousel -->
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="images/caro1.jpg" alt="Valorant" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="images/caro2.jpg" alt="Mobile Legend" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+                <img src="images/caro3.jpg" alt="PUBG" class="d-block w-100">
+            </div>
+        </div>
+    </div>
+
+    <!-- topgame -->
+    <div class="container row">
+        <h2 class="pt-4 ">Top Game!</h2>
+        <p>Our Best offer</p>
+
+        <section class="mx-auto" style="max-width: 23rem;">
+          <div class="card">
+            <div class="card-body d-flex flex-row">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" class="rounded-circle me-3" height="50px"
+                width="50px" alt="avatar" />
+              <div>
+                <h5 class="card-title font-weight-bold mb-2">New spicy meals</h5>
+                <p class="card-text"><i class="far fa-clock pe-2"></i>07/24/2018</p>
+              </div>
+            </div>
+            <div class="bg-image hover-overlay ripple rounded-0" data-mdb-ripple-color="light">
+              <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/full page/2.jpg"
+                alt="Card image cap" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <section class="mx-auto" style="max-width: 23rem;">
+          <div class="card">
+            <div class="card-body d-flex flex-row">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" class="rounded-circle me-3" height="50px"
+                width="50px" alt="avatar" />
+              <div>
+                <h5 class="card-title font-weight-bold mb-2">New spicy meals</h5>
+                <p class="card-text"><i class="far fa-clock pe-2"></i>07/24/2018</p>
+              </div>
+            </div>
+            <div class="bg-image hover-overlay ripple rounded-0" data-mdb-ripple-color="light">
+              <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/full page/2.jpg"
+                alt="Card image cap" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+              </a>
+            </div>
+          </div>
+        </section>
+
+        <section class="mx-auto" style="max-width: 23rem;">
+          <div class="card">
+            <div class="card-body d-flex flex-row">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-8.jpg" class="rounded-circle me-3" height="50px"
+                width="50px" alt="avatar" />
+              <div>
+                <h5 class="card-title font-weight-bold mb-2">New spicy meals</h5>
+                <p class="card-text"><i class="far fa-clock pe-2"></i>07/24/2018</p>
+              </div>
+            </div>
+            <div class="bg-image hover-overlay ripple rounded-0" data-mdb-ripple-color="light">
+              <img class="img-fluid" src="https://mdbootstrap.com/img/Photos/Horizontal/Food/full page/2.jpg"
+                alt="Card image cap" />
+              <a href="#!">
+                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+
+    <!-- Service -->
+    <div class="services col">
+        <h2 class="pt-4">Our Services</h2>
+        <p>Enjoy Our Services</p>
+        <div class="row d-flex justify-content-center gap-4 pt-4">
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="images/serv1.png" class="card-img-top" style="padding-top: 10px;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Valorant</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="images/serv2.png" class="card-img-top" style="padding-top: 10px;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Mobile Legends</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="images/serv3.png" class="card-img-top" style="padding-top: 10px;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">EA FC</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="images/serv4.png" class="card-img-top" style="padding-top: 10px;" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Free Fire</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="images/serv5.png" class="card-img-top" style="padding-top: 10px;" alt="...">
+                    <div class="card-body">
+                <h5 class="card-title d-flex justify-content-center">COD</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center gap-4 pt-4">
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="images/serv6.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv7.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv8.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv9.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv10.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="row d-flex justify-content-center gap-4 pt-4">
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv11.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv12.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv13.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv14.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+            <div class="card d-flex justify-content-center" style="width: 230px;">
+                <img src="{{ URL('image/serv15.png') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title d-flex justify-content-center">Game Name</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- News -->
+    <div class="news pt-4">
+        <h2>News</h2>
+        <p class="pb-3">Hot news about new games and update</p>
+        
+        <div class="container row gx-5">
+            <div class="col-md-6 mb-4">
+                <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+                <img src="images/news1.jpg" class="img-fluid" />
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+                </div>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">News of the day</span>
+                <h4><strong>Facilis consequatur eligendi</strong></h4>
+                <p class="text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
+                eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
+                sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
+                </p>
+                <button type="button" class="btn btn-primary">Read more</button>
+            </div>
+        </div>
+
+        <div class="container row gx-5">
+            <div class="col-md-6 mb-4">
+                <div class="bg-image hover-overlay ripple shadow-2-strong rounded-5" data-mdb-ripple-color="light">
+                <img src="images/news1.jpg" class="img-fluid" />
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+                </div>
+            </div>
+
+            <div class="col-md-6 mb-4">
+                <span class="badge bg-danger px-2 py-1 shadow-1-strong mb-3">News of the day</span>
+                <h4><strong>Facilis consequatur eligendi</strong></h4>
+                <p class="text-muted">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
+                eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
+                sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
+                </p>
+                <button type="button" class="btn btn-primary">Read more</button>
+            </div>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,98 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg p-4" style="background-color: #0B9BB4">
+    <!-- Container wrapper -->
+    <div class="container">
+      <!-- Navbar brand -->
+      <a class="navbar-brand" href="#"><img id="MDB-logo"
+          src="images/gb.png" alt="Game Booster"
+          draggable="false" height="30" style="scale: 250%; margin-right: 40px"/></a>
+  
+      <!-- Toggle button -->
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+        data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+  
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse align-items-center" style="width: 300px" id="navbarSupportedContent">
+        <form class="d-flex align-items-center w-100 form-search">
+          <div class="input-group">
+            <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
+          </div>
+          <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
+        </form>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-3">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                All
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Jockey</a>
+                <a class="dropdown-item" href="#">Top up</a>
+                <div class="dropdown-divider"></div>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center me-3" href="#!">
+                <i class="fas fa-bookmark pe-2"></i> News
+              </a>
+            </li>
+            <li class="nav-item" style="width: 100px;">
+              <a class="nav-link d-flex align-items-center" href="#!">Contact Us</a>
+            </li>
+            <!--  -->
+            <!-- <button class="btn dropdown-toggle shadow-0 bg-transparent" type="button" data-mdb-toggle="dropdown"
+              aria-expanded="false" style="padding-bottom: 0.4rem;">
+              Service
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark fa-ul">
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-search"></i></span>All</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                      class="fas fa-film"></i></span>Jockey</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-tv"></i></span>TV
+                  TopUp</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                      class="fas fa-user-friends"></i></span>Celebs</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                      class="fas fa-building"></i></span>Companies</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                      class="fas fa-key"></i></span>Keywords</a>
+              </li>
+              <li>
+                <hr class="dropdown-divider" />
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i
+                      class="fas fa-search-plus"></i></span>Advanced
+                  search<i class="fas fa-chevron-right ps-2"></i></a>
+              </li>
+            </ul>
+            <li class="nav-item">
+              <a class="nav-link d-flex align-items-center me-3" href="#!">
+                <i class="fas fa-bookmark pe-2"></i> News
+              </a>
+            </li>
+            <li class="nav-item" style="width: 100px;">
+              <a class="nav-link d-flex align-items-center" href="#!">Contact Us</a>
+            </li> -->
+          </ul>
+        </div>
+      </div>
+      <!-- Collapsible wrapper -->
+    </div>
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
